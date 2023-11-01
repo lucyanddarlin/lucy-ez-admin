1581855f8255df766036f26f2263909056892bd5
lucy.li <lucy.li@quectel.com>
2023-11-01 09:20:19 +0800

    feat: 增加 userLoginResponse & 修改服务启动端口号

--- types/user.go
+++ types/user.go
@@ -7,3 +7,7 @@ type UserLoginRequest struct {
 	CaptchaID   string `json:"captcha_id" binding:"required"`
 	Captcha     string `json:"captcha"  binding:"required"`
 }
+
+type UserLoginResponse struct {
+	Token string `json:"token"`
+}
